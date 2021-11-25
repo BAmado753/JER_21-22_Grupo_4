15be80365ed6df786b3bc77528e4571b36e78ef5
BAmado753 <65674008+BAmado753@users.noreply.github.com>
2021-11-25 15:03:38 +0100

    Prueba escenas 1

---
+++ TinyTussel/src/js/scenes/gameBase.js
@@ -0,0 +1,34 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+  //Personajes
+  this.load.image('Chilli', 'assets/Chilli.png');
+  this.load.image('Bernie', 'assets/Bernie.png');
+  this.load.image('Wasabi', 'assets/Wasabi.png');
+
+  //Escenarios
+  this.load.image('escenario1', 'assets/escenario1.png');
+  this.load.image('escenario2', 'assets/escenario2.png');
+  this.load.image('escenario3', 'assets/escenario3.png');
+}
+
+function create ()
+{
+  this.add.image(400, 300, 'escenario1')
+}
+
+function update ()
+{
+}
