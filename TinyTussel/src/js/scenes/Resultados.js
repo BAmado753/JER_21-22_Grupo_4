2f497c0b3f13a9208c964f9a5286aee81c36a4f6
BAmado753 <65674008+BAmado753@users.noreply.github.com>
2021-11-28 17:54:18 +0100

    Actualización escenas
    Inclusión de las escenas puestas en el diagrama de flujo

---
+++ TinyTussel/src/js/scenes/Resultados.js
@@ -0,0 +1,10 @@
+export class Resultados extends Phaser.Scene{
+	constructor(){
+		//PantallaResultados--> nombre que se le da a la escena
+		super({ key: "PantallaResultados"});
+	}
+
+	create(){
+
+	}
+}
