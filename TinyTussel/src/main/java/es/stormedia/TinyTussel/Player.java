c58d81a5309124dd18f8317fe7cfca74b490f531
ClaraNM <claranm00@outlook.es>
2021-12-19 05:39:57 +0100

    Ejemplos de POST,GET y PUT implementados usando json.

--- TinyTussel/src/main/java/es/stormedia/TinyTussel/Player.java
+++ TinyTussel/src/main/java/es/stormedia/TinyTussel/Player.java
@@ -2,20 +2,33 @@ package es.stormedia.TinyTussel;
 
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 
 public class Player {
 	private String name;
 	private int id;
-	
+	private int maxPuntuacion;
+	private String favChara;
+	private int usosChilli;
+	private int usosWasabi;
+	private int usosBernie;
 public Player() {
 		
 		
 	}
-	
-	public Player(String name,int id) {
+	@JsonCreator
+	public Player(@JsonProperty("name") String name,@JsonProperty("id")int id,@JsonProperty("maxPuntuacion") int mP,@JsonProperty("favChara")String fC,
+			@JsonProperty("usosChilli")int uC, @JsonProperty("usosWasabi")int uW,@JsonProperty("usosBernie") int uB) {
 		
 		this.name=name;
 		this.id=id;
+		this.maxPuntuacion=mP;
+		this.favChara=fC;
+		this.usosChilli=uC;
+		this.usosWasabi=uW;
+		this.usosBernie=uB;
 	}
 	public String getName() {
 		return name;
@@ -30,5 +43,36 @@ public Player() {
 
 	public void setId(int id) {
 		this.id = id;
+	}
+	public int getMaxPuntuacion() {
+		return maxPuntuacion;
+	}
+
+	public void setMaxPuntuacion(int maxPuntuacion) {
+		this.maxPuntuacion = maxPuntuacion;
+	}
+	public String getFavChara() {
+		return favChara;
+	}
+	public void setFavChara(String fC) {
+		this.favChara=fC;
+	}
+	public int getUsosChilli() {
+		return usosChilli;
+	}
+	public void setUsosChilli(int usosChilli) {
+		this.usosChilli = usosChilli;
+	}
+	public int getUsosWasabi() {
+		return usosWasabi;
+	}
+	public void setUsosWasabi(int usosWasabi) {
+		this.usosWasabi = usosWasabi;
+	}
+	public int getUsosBernie() {
+		return usosBernie;
+	}
+	public void setUsosBernie(int usosBernie) {
+		this.usosBernie = usosBernie;
 	}
 }
--- TinyTussel/src/main/java/es/stormedia/TinyTussel/PlayerController.java
+++ TinyTussel/src/main/java/es/stormedia/TinyTussel/PlayerController.java
@@ -1,7 +1,12 @@
 package es.stormedia.TinyTussel;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +17,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.core.exc.StreamWriteException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +35,7 @@ import org.springframework.web.bind.annotation.*;
 public class PlayerController {
 	@Autowired private Player player;
 	private List<Player> playerList = new ArrayList<>();
+	
 	private List<String> playerNames = new ArrayList<>();
 	private int id;
 	private void readNames() {
@@ -44,32 +59,58 @@ public class PlayerController {
 	}
 	
 	@PostMapping("/player")
-	public int addPlayer(@RequestBody Player player) throws FileNotFoundException {
+	public int addPlayer(@RequestBody Player player) {
 		player.setId(id);
-		playerList.add(player);
+		//playerList.add(player);
 		id++;
-
-		try {
-			PrintStream flujo;
-			flujo = new PrintStream(new FileOutputStream("jugadores.txt", true));
-
-			flujo.println(player.getName());
-			flujo.close();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+			ObjectMapper mapper= new ObjectMapper();
+			try {
+				mapper.writeValue(new File("player"+player.getId()+".json"), player);
+			} catch (StreamWriteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (DatabindException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
 
 		return player.getId();
 	}
 	
 	@GetMapping("/player/name/{id}")
 	public String getPlayerName(@PathVariable int id) {
+		
+		try {
+			ObjectMapper mapper= new ObjectMapper();
+			InputStream inputStream = new FileInputStream (new File("player"+id+".json"));
+			//System.out.println(inputStream);
+			TypeReference<Player> typeReference= new TypeReference<Player>() {};
+			try {
+				playerList.add( mapper.readValue(inputStream, typeReference));
+				inputStream.close();
+			} catch (StreamReadException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (DatabindException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		if (!playerList.isEmpty())
-			for (int i = 0; i < playerList.size(); i++) {
+			for (Player p: playerList) {
 
-				if (playerList.get(i).getId() == id) {
-					return playerList.get(i).getName();
+				if (p.getId() == id) {
+					return p.getName();
 				}
 
 			}
@@ -77,6 +118,85 @@ public class PlayerController {
 		return null;
 
 	}
+	@GetMapping("/player/maxPuntuacion/{id}")
+	public int getMaxPuntuacion(@PathVariable int id) {
+		
+		try {
+			ObjectMapper mapper= new ObjectMapper();
+			InputStream inputStream = new FileInputStream (new File("player"+id+".json"));
+			//System.out.println(inputStream);
+			TypeReference<Player> typeReference= new TypeReference<Player>() {};
+			try {
+				playerList.add( mapper.readValue(inputStream, typeReference));
+				inputStream.close();
+			} catch (StreamReadException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (DatabindException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (!playerList.isEmpty())
+			for (Player p: playerList) {
+
+				if (p.getId() == id) {
+					return p.getMaxPuntuacion();
+				}
+
+			}
+
+		return 0;
+
+	}
 	
-	
+	@PutMapping("/player/name/{id}")
+	public String actualizaNombre (@PathVariable int id , @RequestBody String newName ){
+
+		try {
+			ObjectMapper mapper= new ObjectMapper();
+			InputStream inputStream = new FileInputStream (new File("player"+id+".json"));
+			//System.out.println(inputStream);
+			TypeReference<Player> typeReference= new TypeReference<Player>() {};
+			try {
+				Player player= mapper.readValue(inputStream, typeReference);
+				inputStream.close();
+			} catch (StreamReadException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (DatabindException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			player.setName(newName);
+			try {
+				mapper.writeValue(new File("player"+id+".json"), player);
+			} catch (StreamWriteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (DatabindException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+					return player.getName();
+				
+
+		}
 }
